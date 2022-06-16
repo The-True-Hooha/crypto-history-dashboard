f2a319cd9c12b44168e564c857af463d3d4c44fa
David Ogar <owogogahhero@outlook.com>
2022-06-16 12:57:41 +0100

    initial commit: adding files to remote

---
+++ src/main/java/com/github/TheTrueHooha/cryptohistorydashboard/CryptoHistoryDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.cryptohistorydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoHistoryDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoHistoryDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/TheTrueHooha/cryptohistorydashboard/CryptoHistoryDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.cryptohistorydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoHistoryDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
