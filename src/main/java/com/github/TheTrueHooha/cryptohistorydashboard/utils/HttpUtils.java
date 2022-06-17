5d72e47e810c964326b0d40b6ab95d8f6ec2379d
David Ogar <owogogahhero@outlook.com>
2022-06-17 03:59:48 +0100

    add http utils config

---
+++ src/main/java/com/github/TheTrueHooha/cryptohistorydashboard/utils/HttpUtils.java
@@ -0,0 +1,23 @@
+package com.github.TheTrueHooha.cryptohistorydashboard.utils;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.util.Collections;
+
+public class HttpUtils {
+
+    private static  String apiHost = "coinranking1.p.rapidapi.com";
+    private static  String apiKey = "bddd8711c8msh172c4aba06bbf60p1a8473jsne113e831e770";
+
+    public static HttpEntity<String> getHttpEntity(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        httpHeaders.set("X-RapidAPI-Host", apiHost);
+        httpHeaders.set("X-RapidAPI-Key", apiKey);
+        return new HttpEntity<>(null, httpHeaders);
+    }
+
+
+}
