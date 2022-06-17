ef7f306028da741f25bb356fff58816fe16635d1
David Ogar <owogogahhero@outlook.com>
2022-06-17 14:32:08 +0100

    feat: add coins model

---
+++ src/main/java/com/github/TheTrueHooha/cryptohistorydashboard/model/coins/Coins.java
@@ -0,0 +1,12 @@
+package com.github.TheTrueHooha.cryptohistorydashboard.model.coins;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Coins {
+    private String status;
+    private CoinData data;
+}
